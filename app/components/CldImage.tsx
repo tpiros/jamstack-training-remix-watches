51fef90dc2552c89be0a429ee1b610dfc328fec4
tpiros <2042718+tpiros@users.noreply.github.com>
2023-03-24 10:54:22 +0800

    first commit

---
+++ app/components/CldImage.tsx
@@ -0,0 +1,31 @@
+import { Cloudinary } from '@cloudinary/url-gen';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
+import { face } from '@cloudinary/url-gen/qualifiers/focusOn';
+import { format, quality } from '@cloudinary/url-gen/actions/delivery';
+import { AdvancedImage } from '@cloudinary/react';
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'tamas-demo',
+  },
+});
+interface Params {
+  publicId: string;
+}
+const CldImage = ({ publicId }: Params) => {
+  const myImage = cld
+    .image(publicId)
+    .resize(
+      thumbnail().width(250).height(250).zoom(0.75).gravity(focusOn(face()))
+    )
+    .delivery(format('auto'))
+    .delivery(quality('auto'));
+  return (
+    <AdvancedImage
+      cldImg={myImage}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+  );
+};
+export default CldImage;
